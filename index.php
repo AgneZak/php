23daa2a68c1b696e8cfbf8286f40d1cac7d86013
Engaz01 <azakaite@github.com>
2020-10-15 16:41:22 +0300

    leap year code

--- index.php
+++ index.php
@@ -1,36 +1,18 @@
 <?php
-    $name = 'Agne';
-    $age = rand(15,35);
-    $teistumas = rand(0,1);
-    $kids = rand(1,5);
+    $years = rand(1991,2020);
 
-    if($teistumas){
-        $teistumas_tekstas = 'teista';
+    if($years % 4 == 0 && ($years % 100 != 0 || $years % 400 == 0)){
+        $h2 = "{$years} is leap year";
     } else{
-        $teistumas_tekstas = 'neteistas';
+        $h2 = "{$years} is not leap year";
     }
-
-    if ($age > 25 || $age < 18 || $teistumas || $kids > 2){
-        $h2 = 'nėra šaukiamas į kariuomenę, nes ';
-        if($age > 25){
-            $h2 .= "per sena"; 
-        } if($age < 18){
-            $h2 .= "per jauna";
-        } if($kids > 2){
-            $h2 .= " turi {$kids} vaikus";
-        } if($teistumas){
-            $h2 .= " {$teistumas_tekstas}";
-        }
-    } else {
-        $h2 = 'yra šaukiamas į kariuomenę'; 
-    }
-
+    
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Kariuomene</title>
+    <title>Leap Year</title>
 </head>
 <style>
 body{
@@ -43,13 +25,8 @@ body{
 
 </style>
 <body>
-    <ul>
-        <li><?php print $name;?></li>
-        <li><?php print $age;?></li>
-        <li>Turi vaiku:<?php print $kids;?></li>
-        <li>Teistumas:<?php print $teistumas_tekstas;?></li>
-    </ul>
-    <h2><?php print $name. ' '.$h2; ?></h2>
+    
+    <h2><?php print $h2; ?></h2>
     
     
 </body>
