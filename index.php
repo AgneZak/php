525d6457095c23cdafb145fc5e8050a2eba46b6a
Engaz01 <azakaite@github.com>
2020-10-15 10:45:22 +0300

    weather updated

--- index.php
+++ index.php
@@ -1,23 +1,22 @@
 <?php
 $sunny = rand(0,1);
 $rainy = rand(0,1);
-$weather = ' ';
 
 if ($sunny) {
     if($rainy){
         $h2 = "It's rainy with clouds";
-        $weather = "sun-rain";
+        $weather_class = "weather--sun-rain";
     } else {
         $h2 = "It's sunny";
-        $weather = "sunny";
+        $weather_class = "weather--sunny";
     }
 } else {
     if(!$rainy){
         $h2 = "It's cloudy";
-        $weather = "cloudy";
+        $weather_class = "weather--cloudy";
     }else{
         $h2 = "It's rainy";
-        $weather = "rainy";
+        $weather_class = "weather--rainy";
     }
 }
 ?>
@@ -28,34 +27,27 @@ if ($sunny) {
     <title>Oras</title>
 </head>
 <style>
-.rainy{
+.weather{
+    background-size: cover;
+    width:50px;
+    height:50px;
+}
+.weather--rainy{
     background-image: url(https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather07-512.png);
-    height:50px;
-    width:50px;
-    background-size: cover;
 }
-.sunny{
+.weather--sunny{
     background-image: url(https://www.pinclipart.com/picdir/middle/5-51534_clipart-of-a-sun-sunny-icon-png-download.png);
-    height:50px;
-    width:50px;
-    background-size: cover;
 }
-.sun-rain{
+.weather--sun-rain{
     background-image: url(https://toppng.com/uploads/preview/sun-behind-rain-cloud-icon-cloud-and-rain-transparent-ico-11562909339q8plfa1nfd.png);
-    height:50px;
-    width:50px;
-    background-size: cover;
 }
-.cloudy{
+.weather--cloudy{
     background-image: url(https://png.pngtree.com/png-vector/20190214/ourlarge/pngtree-vector-cloudy-icon-png-image_450295.jpg);
-    height:50px;
-    width:50px;
-    background-size: cover;
 }
 
 </style>
 <body>
-    <div class="<?php print $weather; ?>"></div>
+    <div class="weather <?php print $weather_class; ?>"></div>
     <h2><?php print $h2; ?></h2>
 </body>
 </html>
