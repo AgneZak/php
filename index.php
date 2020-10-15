993808d157358e76dd1cb65c348f02bdfc6363d4
Engaz01 <azakaite@github.com>
2020-10-15 15:33:25 +0300

    joint code needs change in px

--- index.php
+++ index.php
@@ -1,63 +1,53 @@
 <?php
-    date_default_timezone_set('Europe/Vilnius');
-    $hours = date('h')*30;
-    $minutes = date('i')*6;
-    $seconds = date('s')*6;
+    $minutes = abs(date('i')-59);
+    $minutes_remain = $minutes % 5;
+
+    $seconds = abs(date('s')-59);    
+    $percents = $minutes_remain.$seconds / 60 * 100;
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Clock</title>
+    <title>Joint'o Load</title>
 </head>
 <style>
 body{
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     height: 100vH;
 }
-.clock{
-    background-image: url(https://lh3.googleusercontent.com/proxy/qIJqnxKSIZGeQfnhPokRMHI0kYJ-blISVvBkXp-VlwW_Uj3bfsWs1OPF3rWX59Hy7diyHlaLQpvG5Hr4rc5qjSanuYzCqD7aktJk-9MxCRgW7Zs54oQ_kUX3mdIPMUF8dUmfmgoWQWyDlcOKBOU);
+.joint-smoked{
+    background-image: url(https://banner2.cleanpng.com/20190807/hoc/kisspng-drawing-joint-blunt-cannabis-design-5d4a4f196c73d6.5354688115651510014442.jpg);
+    background-size: cover;
+    width:<?php print $percents; ?>px;
+    height:400px;
+    display: flex;
+    justify-content: center;
+    position: absolute;
+    left:0;
+    
+}
+.joint{
+    position:relative;
+    background-image: url(https://w7.pngwing.com/pngs/140/121/png-transparent-joint-medical-cannabis-blunt-deal-with-it-angle-fashion-auto-part-thumbnail.png);
     background-size: cover;
     width:400px;
     height:400px;
     display: flex;
     justify-content: center;
-    align-items: baseline;
 }
-.hours{
-    background-color: black;
-    height:180px;
-    width:10px;
-    transform: rotate(<?php print $hours;?>deg);
-    transform-origin: left center;
-    border-radius:20%;
-}
-.minutes{
-    background-color: red;
-    height:150px;
-    width:5px;
-    transform: rotate(<?php print $minutes;?>deg);
-    transform-origin: 10% 100%;
-    border-radius:20%;
-}
-.seconds{
-    background-color: blue;
-    height:200px;
-    width:5px;
-    transform: rotate(<?php print $seconds;?>deg);
-    transform-origin: 10% 100%;
-    border-radius:20%;
-}
+
 </style>
 <body>
-    <div class="clock">
-        <div class='hours'></div>
-        <div class='minutes'></div>
-        <div class='seconds'></div>
+    <div class="joint">
+        <div class='joint-smoked'></div>
     </div>
+    
+    <p>Liko <?php print $minutes_remain . ':' .$seconds; ?> </p>
     
 </body>
 </html>
