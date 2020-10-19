1c486a9f8b96cfc16b84ae3b0b67242c54a98cc8
Engaz01 <azakaite@github.com>
2020-10-19 15:49:42 +0300

    commiting daugybos koda;

--- index.php
+++ index.php
@@ -1,32 +1,33 @@
 <?php
-    $random_seconds = rand(0,100000);
-    $hours = floor($random_seconds/3600);
-    $minutes = floor(($random_seconds/60)%60);
-    $seconds = $random_seconds%60;
-    
-    
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Sekundziu skaiciuotuvas</title>
+    <title>Ciklas</title>
 </head>
 <style>
 body{
     display: flex;
-    flex-direction: column;
     justify-content: center;
     align-items: center;
-    height: 100vH;
+    /* height: 100vH; */
 }
-
+div{
+    margin:10px;
+    border: 1px solid black;
+    padding: 10px;
+}
 </style>
 <body>
-    
-    <h2>Is viso sekundziu: <?php print $random_seconds; ?></h2>
-    <h3><?php print $hours.'h'.$minutes.'m'.$seconds ; ?>s</h3>
-    
-    
+    <?php for($x = 1; $x <= 9; $x++):?>
+    <div>
+        <?php for($i = 1; $i <= 9; $i++):?>
+       <p><?php print $x .'x'.$i."=".$math=$i*$x;?></p>
+        <?php endfor;?>
+    </div>
+    <?php endfor;?>
 </body>
 </html>
