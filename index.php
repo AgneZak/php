b9025661157aa33b5199f164d7d0e89d501b80d6
Engaz01 <azakaite@github.com>
2020-10-19 16:36:36 +0300

    chess code

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+$condition = 4%2 == 0;
 
 ?>
 <!DOCTYPE html>
@@ -11,23 +11,31 @@
 <style>
 body{
     display: flex;
-    justify-content: center;
-    align-items: center;
+    /* justify-content: center;
+    align-items: center; */
     /* height: 100vH; */
 }
 div{
-    margin:10px;
     border: 1px solid black;
-    padding: 10px;
+    height:30px;
+    width:30px;
+}
+.black{
+    background-color:black;
 }
 </style>
 <body>
-    <?php for($x = 1; $x <= 9; $x++):?>
+    <?php for($x = 1; $x <= 8; $x++):?>
     <div>
-        <?php for($i = 1; $i <= 9; $i++):?>
-       <p><?php print $x .'x'.$i."=".$math=$i*$x;?></p>
+        <?php for($i = 1; $i <= 8; $i++):?>
+        <?php if(($x + $i) % 2 == 0){ ?>
+          <div class='black'></div>
+          <?php } else { ?>
+         <div></div>
+         <?php } ?>
         <?php endfor;?>
     </div>
     <?php endfor;?>
+   
 </body>
 </html>
