fea8749c55d95f624cc98dbdea4b1e9c790242f5
Engaz01 <azakaite@github.com>
2020-10-21 10:13:44 +0300

    used car procents

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
+$months = 24;
+$car_price_new = 30000;
+$depreciation = 2;
+$car_price_used = 30000;
 
+for ($i = 0; $i < $months; $i++) { 
+    $car_price_used *= (100 - $depreciation)/100;  
+}
 
+$car_price_used_f = number_format($car_price_used, 2);
+$depr_perc = number_format((1 - $car_price_used/$car_price_new)*100, 2);
+
+$h2 = "Naujos masinos kaina: $car_price_new";
+$h3 = "Po $months men., masinos verte bus: $car_price_used_f eur.";
+$h4 = "Masina nuvertes $depr_perc prc.";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,19 +28,12 @@ body{
     justify-content: center; 
     align-items: center;   
 }
-div{
-    width: 300px;
-    height: 25px;
-}
-<?php for($i =1; $i <= 15; $i++):?>
-    <?php print ".color-$i";?>{
-        background-color: rgb(<?php print 255*($i/15);?>,0,0);
-    }
-<?php endfor; ?>
+
 </style>
 <body>
-    <?php for($i =1; $i <= 15; $i++):?>
-    <div class = "color-<?php print $i;?>"></div>
-    <?php endfor; ?>
+    <h1>Kiek nuvertes masina?</h1>
+    <h2><?php print $h2;?></h2>
+    <h3><?php print $h3;?></h3>
+    <h4><?php print $h4;?></h4>
 </body>
 </html>
