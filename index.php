456da037f019c919e286328c9d7481cfb7382998
Engaz01 <azakaite@github.com>
2020-10-26 10:57:23 +0200

    episodes task

--- index.php
+++ index.php
@@ -1,39 +1,50 @@
 <?php
-$months = 24;
-$car_price_new = 30000;
-$depreciation = 2;
-$car_price_used = 30000;
+$serialas = [
+    [
+        'year' => 2020,
+        'episodes' => rand (20,25),
+        'duration' => rand (20,25)
+    ],
+    [
+        'year' => 2019,
+        'episodes' => rand (20,25),
+        'duration' => rand (20,25)
 
-for ($i = 0; $i < $months; $i++) { 
-    $car_price_used *= (100 - $depreciation)/100;  
+
+    ],
+    [
+        'year' => 2018,
+        'episodes' => rand (20,25),
+        'duration' => rand (20,25)
+
+    ]
+];
+$ziuriu_per_diena = 60;
+
+$how_much_episodes = 0;
+$how_long = 0;
+foreach ($serialas as $key => $value) {
+    $how_much_episodes += $value['episodes'];
+    $how_long += $value['duration']*$value['episodes'];
 }
+$days_f = number_format($how_long / $ziuriu_per_diena, 2);
 
-$car_price_used_f = number_format($car_price_used, 2);
-$depr_perc = number_format((1 - $car_price_used/$car_price_new)*100, 2);
 
-$h2 = "Naujos masinos kaina: $car_price_new";
-$h3 = "Po $months men., masinos verte bus: $car_price_used_f eur.";
-$h4 = "Masina nuvertes $depr_perc prc.";
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Ciklas</title>
+    <title>Array</title>
 </head>
 <style>
-body{
-    display: flex;
-    flex-direction: column;
-    justify-content: center; 
-    align-items: center;   
-}
 
 </style>
-<body>
-    <h1>Kiek nuvertes masina?</h1>
-    <h2><?php print $h2;?></h2>
-    <h3><?php print $h3;?></h3>
-    <h4><?php print $h4;?></h4>
+<body>  
+<section>
+<p><?php print $how_much_episodes; ?> epizodu is viso</p>
+<p><?php print $how_long; ?> minutes </p>
+<p><?php print $days_f; ?> dienu</p>
+</section>   
 </body>
 </html>
