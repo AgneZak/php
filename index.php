e7d0144401ea689b97cbb5643e6c4dd7ed4cdd50
Engaz01 <azakaite@github.com>
2020-10-28 10:46:44 +0200

    prime finder code

--- index.php
+++ index.php
@@ -1,20 +1,47 @@
 <?php
-$cube = [
-    [0,1,1],
-    [1,0,1],
-    [0,1,0]
-];
-$new_cube = [];
-foreach ($cube as $cube_key => $number) {
-    foreach ($number as $number_key => $value) {
-        if($value === 0) {
-            $new_cube[$cube_key][$number_key] = 1;
-        } else {
-            $new_cube[$cube_key][$number_key] = 0;
-        };
+$x = rand(1,100);
+$y = rand(1,100);
+
+function is_prime($number) {
+    if ($number === 1) {
+        return false;
+    }
+    for ($i = 2; $i <= $number/2; $i++) {
+        if ($number % $i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+$answer = is_prime($x);
+$answer_two = is_prime($y);
+
+if ($answer) {
+    $p_x = "$x is prime";
+} else {
+    $p_x = "$x is not prime";
+}
+
+if ($answer_two) {
+    $p_y = "$y is prime";
+} else {
+    $p_y = "$y is not prime";
+}
+
+function sum_if_prime($x, $y) {
+    if (is_prime($x) && is_prime($y)) {
+        return $x + $y;
     }
 }
-var_dump($new_cube)
+
+$sum = sum_if_prime($x, $y);
+
+if ($sum) {
+    $p_sum ="The sum of prime is $sum";
+} else {
+    $p_sum ="The sum of prime is $sum";
+}
 ?>
 
 <!DOCTYPE html>
@@ -24,10 +51,13 @@ var_dump($new_cube)
     <title>Function</title>
 </head>
 <style>
-
+    
 </style>
 <body> 
 <main>
+    <p><?php print $p_x; ?></p>
+    <p><?php print $p_y; ?></p>
+    <p><?php print $p_sum; ?></p>
 </main>
 </body>
 </html>
