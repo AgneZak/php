9a7923c41b40166b326842d525689bcd1f7793a1
Engaz01 <azakaite@github.com>
2020-11-30 09:51:39 +0200

    Add change to install.php so it loads once

--- public_html/install.php
+++ public_html/install.php
@@ -1,6 +1,7 @@
 <?php
 require '../bootloader.php';
 
+App::$db = new FileDB(DB_FILE);
 
 App::$db->createTable('users');
 App::$db->insertRow('users', ['email' => 'test@test.lt', 'password' => 'test']);
