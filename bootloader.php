46365da9ed28fc2f28b6984e3d9829b4421ad476
Engaz01 <azakaite@github.com>
2020-12-08 12:47:17 +0200

    Add update to bootloader, deleting unused requires

--- bootloader.php
+++ bootloader.php
@@ -7,10 +7,8 @@ define('DB_FILE', ROOT . '/app/data/db.json');
 
 require 'core/functions/html.php';
 require 'core/functions/form/validators.php';
-require 'core/functions/file.php';
 require 'core/classes/FileDB.php';
 require 'app/functions/form/validators.php';
-require 'app/functions/form/auth.php';
 require 'app/classes/App.php';
 require 'vendor/autoload.php';
 
