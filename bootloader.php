f9d3e39c3c51c0a4b52f7911aaab337f20610b09
Engaz01 <azakaite@github.com>
2020-12-08 12:50:07 +0200

    Add deleting unused requires

--- bootloader.php
+++ bootloader.php
@@ -7,9 +7,10 @@ define('DB_FILE', ROOT . '/app/data/db.json');
 
 require 'core/functions/html.php';
 require 'core/functions/form/validators.php';
-require 'core/classes/FileDB.php';
+
 require 'app/functions/form/validators.php';
 require 'app/classes/App.php';
+
 require 'vendor/autoload.php';
 
 $app = new App();
